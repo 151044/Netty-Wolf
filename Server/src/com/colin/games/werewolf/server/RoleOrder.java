4b0348d30a3278371223bf0a7f5aa26eb5a7f0d0
151044 <151044@std.stpaulscollege.edu.hk>
2020-05-27 22:55:56 +0800

    Begin role order

---
+++ Server/src/com/colin/games/werewolf/server/RoleOrder.java
@@ -0,0 +1,50 @@
+/*
+ * Netty-Wolf
+ * Copyright (C) 2020  Colin Chow
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package com.colin.games.werewolf.server;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class RoleOrder {
+    private RoleOrder(){
+        throw new AssertionError();
+    }
+    private static Map<String,String> roleToCallback = new HashMap<>();
+    private static List<String> order = new ArrayList<>();
+    public static void setBefore(String before,String role,String callback){
+        int pos = order.indexOf(before);
+        if(pos == -1){
+            order.add(0,role);
+        }else{
+            order.add(pos - 1,role);
+        }
+        roleToCallback.put(role,callback);
+    }
+    public static void setAfter(String after,String role,String callback){
+        int pos = order.indexOf(after);
+        if(pos == -1){
+            order.add(0,role);
+        }else{
+            order.add(pos,role);
+        }
+        roleToCallback.put(role,callback);
+    }
+}
