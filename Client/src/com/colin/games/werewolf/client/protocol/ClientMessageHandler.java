12843bc3885ffc1f594e8ba76f43470e919b787c
151044 <151044@std.stpaulscollege.edu.hk>
2020-05-30 16:58:29 +0800

    Adding debug into ClientMessageHandler

--- Client/src/com/colin/games/werewolf/client/protocol/ClientMessageHandler.java
+++ Client/src/com/colin/games/werewolf/client/protocol/ClientMessageHandler.java
@@ -1,3 +1,21 @@
+/*
+ * Netty-Wolf
+ * Copyright (C) 2020  Colin Chow
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 package com.colin.games.werewolf.client.protocol;
 
 import com.colin.games.werewolf.common.message.Message;
@@ -6,9 +24,13 @@ import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInboundHandlerAdapter;
 
 public class ClientMessageHandler extends ChannelInboundHandlerAdapter {
+    private static boolean debug = true;
     @Override
     public void channelRead(ChannelHandlerContext ctx, Object msg) {
         Message m = (Message) msg;
+        if(debug) {
+            System.out.println(m.getContent() + " " + m.getType());
+        }
         MessageDispatch.dispatch(ctx,m);
     }
 }
