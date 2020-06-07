0cb2c7d991182ced83f797fb199dbea8b4c272f2
151044 <151044@std.stpaulscollege.edu.hk>
2020-06-07 13:37:29 +0800

    Work on API for mods started

--- Common/src/com/colin/games/werewolf/common/modding/ModLoader.java
+++ Common/src/com/colin/games/werewolf/common/modding/ModLoader.java
@@ -18,8 +18,27 @@
 
 package com.colin.games.werewolf.common.modding;
 
+import com.colin.games.werewolf.common.utils.ReflectUtils;
+
+import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
+import java.nio.file.Path;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class ModLoader {
     private ModLoader(){
         throw new AssertionError();
+    }
+    public static List<Mod> loadMods() throws IOException, ClassNotFoundException {
+        List<Mod> mods = ReflectUtils.loadConcreteSubclasses(Path.of("./mods"),Mod.class).stream().map(cls -> {
+            try {
+                return cls.getConstructor().newInstance();
+            } catch (InstantiationException | NoSuchMethodException | InvocationTargetException | IllegalAccessException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }).filter(mod -> mod != null).collect(Collectors.toList());
+        return mods;
     }
 }
