841d1a9a9557fda803c273ad5abe7fad7123b511
151044 <51814503+151044@users.noreply.github.com>
2020-11-10 12:18:18 +0800

    Update ChatPane.java

--- Client/src/com/colin/games/werewolf/client/gui/ChatPane.java
+++ Client/src/com/colin/games/werewolf/client/gui/ChatPane.java
@@ -115,7 +115,7 @@ public class ChatPane extends JPanel {
                 SwingUtilities.invokeLater(() -> JOptionPane.showMessageDialog(null, "Message cannot be entered!", "Invalid message!", JOptionPane.WARNING_MESSAGE));
                 return;
             }else if(msg.getText().equals("")){
-                SwingUtilities.invokeLater(() -> JOptionPane.showMessageDialog(null, "Empty message!", "Invalid message!", JOptionPane.WARNING_MESSAGE));
+                SwingUtilities.invokeLater(() -> JOptionPane.showMessageDialog(null, "An empty message cannot be sent!", "Invalid message!", JOptionPane.WARNING_MESSAGE));
                 return;
             }
             String output = "[" + name + "]: " + msg.getText();
