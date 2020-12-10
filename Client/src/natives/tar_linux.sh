831d88e681de356030e3e02138d207ce1bcb0065
151044 <151044@std.stpaulscollege.edu.hk>
2020-12-10 22:54:37 +0800

    Add Linux tar

---
+++ Client/src/natives/tar_linux.sh
@@ -0,0 +1,24 @@
+#
+# Netty-Wolf
+# Copyright (C) 2020  Colin Chow
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#
+
+mkdir make_linux
+mv libaudio.a libaudio.so.1.0.1 make_linux
+mv Audio.h make_linux
+tar -cvzf linux_dev.tar.gz make_linux
+rm -r make_linux
+
